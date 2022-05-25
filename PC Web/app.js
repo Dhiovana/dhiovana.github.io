4595051a7a9d5c1e82bcf75d43bf2ed534be5892
Your Name <you@example.com>
2022-05-25 13:21:28 -0300

    trabalho 1

--- PC Web/app.js
+++ PC Web/app.js
@@ -1,4 +1,4 @@
-const effects = document.getElementById("effects");
+const effects = document.getElementById("filter");
 const effectIntensity = document.getElementById("effectIntensity");
 
 const getFilter = () => effects.value ?? 'none' 
@@ -12,11 +12,13 @@ const getMetric = effect => {
 }
 
 const setFilter = () => {
+    console.log("entrou na funcao")
     const appliedFilter = getFilter()
     const valueIntensity = getIntensity() + getMetric(appliedFilter);
-    const image = document.getElementById("image");
+    const image = document.getElementById("image"); 
+    console.log(`${appliedFilter}(${valueIntensity})`)
     image.style.filter = `${appliedFilter}(${valueIntensity})`
 }
 
-effectIntensity.addEventListener("click", setFilter)
-effects.addEventListener("click", setFilter)
+effectIntensity.addEventListener("change", setFilter)
+effects.addEventListener("change", setFilter)

5c79251b5aac1781ad19f3f4e104623ba94f76b5
Your Name <you@example.com>
2022-06-01 13:32:10 -0300

    exercício 2 adicionado

--- PC Web/trabalho_2/app.js
+++ PC Web/trabalho_2/app.js
@@ -2,3 +2,15 @@ function sum(num1, num2) {
     var DECIMAL = 10;
     return parseInt(num1, DECIMAL) + parseInt(num2, DECIMAL);
 }
+
+-checar num(numEntrada);
+numEntrada = parseFloat(numEntrada);
+if (isNaN(numEntrada)) return 0;
+return numEntrada;
+
+function maior(num1, num2) {
+    if (n1 > n2)
+        return n1
+    else
+        return n2
+} 

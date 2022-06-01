da596dd08e9b22d8b5dcbff64221854e2cdf04e6
Your Name <you@example.com>
2022-06-01 12:47:24 -0300

    javascript adicionado

--- PC Web/trabalho_2/app.js
+++ PC Web/trabalho_2/app.js
@@ -1,5 +1,4 @@
-function sum( number1, number2 ) {
-    var DECIMAL = 10;
-    return parseInt( number1, DECIMAL ) + parseInt( number2, DECIMAL );
-  }
-  
+function sum(num1, num2) {
+    var DECIMAL = 20;
+    return parseInt(num1, DECIMAL) + parseInt(num2, DECIMAL);
+}

251f4dc8316a459cde7160e74f3264c45999da08
Dhiovana Lyssa <88908020+Dhiovana@users.noreply.github.com>
2023-01-05 14:14:54 -0300

    Add files via upload

--- PC Web/ContRestaurante/js/app.js
+++ PC Web/ContRestaurante/js/app.js
@@ -3,25 +3,23 @@ window.onload = async () => {
         .then(async response => await response.json())
         .then(async responseJSON => {
             responseJSON.forEach((item) => {
-                createItem(item)
+                criaItem(item)
             })
         })
 }
-
-const createItem = (item) => {
+const criaItem = (item) => {
     const items = document.querySelector(".items")
     const name = typeof item.name !== 'undefined' ? item.name : item.title
     const prices = item.price.split('$')
-    console.log(prices)
-    const div = `<div class="col-3 mb-5">
-                    <div class="card h-100" style="width: 18rem;">
-                        <img src="${'https://raw.githubusercontent.com/rafaelescalfoni/rafaelescalfoni.github.io/main/desenv_web/restaurante/' + item.photo}" class="card-img-top" alt="${name}">
-                        <div class="card-body d-flex align-items-start flex-column">
-                            <h5 class="card-title">${name}</h5>
-                            <p class="card-text">${item.details}</p>
-                                <a href="#" class="btn btn-danger mt-auto">${item.price}</a>   
-                        </div>
-                    </div>
-                </div>`
+    const div = `<div class="col-4 mb-4">
+                 <div class="card h-30" style="width: 17rem;">
+                     <img src="${'https://raw.githubusercontent.com/rafaelescalfoni/rafaelescalfoni.github.io/main/desenv_web/restaurante/' + item.photo}" class="card-img-top" alt="${name}">
+                     <div class="card-body d-flex align-items-start flex-column">
+                         <h2 class="card-title">${name}</h2>
+                         <h4 class="card-text">${item.details}</h4>
+                              <a href="#" class="btn btn-danger mt-auto">${item.price}</a>   
+                     </div>
+                </div>
+            </div>`
     items.innerHTML += div
 }
